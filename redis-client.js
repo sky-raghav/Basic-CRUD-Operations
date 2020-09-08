b3534785b97432212eb041913fabe04e468f63e7
Akash Raghav <32536112+sky-raghav@users.noreply.github.com>
2020-09-08 15:20:31 +0530

    Update redis-client.js

--- redis-client.js
+++ redis-client.js
@@ -1,9 +1,9 @@
 const redis = require('redis');
 
 let client = redis.createClient({
-  url: process.env.DB_HOST || 'redis://redis-12992.c11.us-east-1-3.ec2.cloud.redislabs.com:12992',
+  url: process.env.DB_HOST,
   no_ready_check: true,
-  auth_pass: process.env.DB_PASSWORD || 'GEIch6P6j3hqri61we8AFoyy57nVYzJk'
+  auth_pass: process.env.DB_PASSWORD
 });
 
 //let client = redis.createClient();
